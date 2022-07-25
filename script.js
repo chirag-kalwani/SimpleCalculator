3eeb2b223c97db33fcda123f4c5403ea09759d8a
Chirag <chiragkalwani@gmail.com>
2022-07-26 00:04:58 +0530

    Fix for decimal not working with external keyboard.

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ for (let button of buttons) {
 
 
 // By using Key-Board KeyPad 
-let keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', ')', '(', 'Enter', 'Escape', 'Backspace'];
+let keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', ')', '(', 'Enter', 'Escape', 'Backspace', '.'];
 document.onkeydown = function (e) {
     if (keys.includes(e.key)) {
         switch (e.key) {
